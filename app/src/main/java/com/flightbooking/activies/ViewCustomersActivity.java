34eb400db59fa34c132d6732bffbff9af1da50d8
iamchaudharysudhir <chaudhary.sk2208@2gmail.com>
2021-02-19 12:27:37 -0500

    view customers Activity added

---
+++ app/src/main/java/com/flightbooking/activies/ViewCustomersActivity.java
@@ -0,0 +1,16 @@
+package com.flightbooking.activies;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.flightbooking.R;
+
+public class ViewCustomersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_customers);
+    }
+}
