3c0bc58007740ca6a6fe36e7c8f430b32070432b
akash1202 <anaghanakash007@gmail.com>
2021-02-19 01:04:11 -0500

    flight booking added

--- app/src/main/java/com/flightbooking/activies/FlightBookingActivity.java
+++ app/src/main/java/com/flightbooking/activies/FlightBookingActivity.java
@@ -9,10 +9,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.flightbooking.R;
-import com.flightbooking.adapters.AvailableFlightsAdapter;
+import com.flightbooking.adapters.FlightBookingAdapter;
 import com.flightbooking.api.ApiService;
 import com.flightbooking.api.RetroClient;
-import com.flightbooking.model.AvailableFlightsPojo;
+import com.flightbooking.model.RouteInfoPojo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,59 +21,51 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class AvailableFlightsActivity extends AppCompatActivity {
-    List<AvailableFlightsPojo> availableFlightsPojo;
+public class FlightBookingActivity extends AppCompatActivity {
+    List<RouteInfoPojo> availableFlightsPojo;
     ListView list_view;
     ProgressDialog progressDialog;
-    String adult,children,classtype;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_available_flights);
 
-        getSupportActionBar().setTitle("Available Flights");
+        getSupportActionBar().setTitle("Flight Booking");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         list_view = (ListView) findViewById(R.id.list_view);
 
-        adult=getIntent().getStringExtra("adult");
-        children=getIntent().getStringExtra("children");
-        classtype=getIntent().getStringExtra("classtype");
-
         availableFlightsPojo = new ArrayList<>();
         getRouteInfo();
     }
     public void getRouteInfo(){
-        progressDialog = new ProgressDialog(AvailableFlightsActivity.this);
+        progressDialog = new ProgressDialog(FlightBookingActivity.this);
         progressDialog.setMessage("Loading....");
         progressDialog.show();
 
         ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
-        Call<List<AvailableFlightsPojo>> call = service.getAvailableFlightRoutes(getIntent().getStringExtra("source"),
-                getIntent().getStringExtra("destination"),getIntent().getStringExtra("type"));
-        call.enqueue(new Callback<List<AvailableFlightsPojo>>() {
+        Call<List<RouteInfoPojo>> call = service.getroutes();
+        call.enqueue(new Callback<List<RouteInfoPojo>>() {
             @Override
-            public void onResponse(Call<List<AvailableFlightsPojo>> call, Response<List<AvailableFlightsPojo>> response) {
+            public void onResponse(Call<List<RouteInfoPojo>> call, Response<List<RouteInfoPojo>> response) {
                 progressDialog.dismiss();
                 if(response.body()==null){
-                    Toast.makeText(AvailableFlightsActivity.this,"No data found",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FlightBookingActivity.this,"No data found",Toast.LENGTH_SHORT).show();
                 }
                 else if (response.body().size() == 0){
-                    Toast.makeText(AvailableFlightsActivity.this,"No data found",Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(FlightBookingActivity.this,"No data found",Toast.LENGTH_SHORT).show();
                 }
                 else {
                     availableFlightsPojo= response.body();
-                    list_view.setAdapter(new AvailableFlightsAdapter(availableFlightsPojo,adult,children,classtype, AvailableFlightsActivity.this));
-
+                    list_view.setAdapter(new FlightBookingAdapter(availableFlightsPojo, FlightBookingActivity.this));
                 }
             }
             @Override
-            public void onFailure(Call<List<AvailableFlightsPojo>> call, Throwable t) {
+            public void onFailure(Call<List<RouteInfoPojo>> call, Throwable t) {
                 progressDialog.dismiss();
-                Toast.makeText(AvailableFlightsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(FlightBookingActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
         });
     }
