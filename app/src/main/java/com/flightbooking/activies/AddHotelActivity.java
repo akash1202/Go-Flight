7e062aa0b9eb04ce2c74e86053fb6f1925c8ede2
iamchaudharysudhir <chaudhary.sk2208@gmail.com>
2021-03-05 13:07:17 -0500

    add hotel activity added

---
+++ app/src/main/java/com/flightbooking/activies/AddHotelActivity.java
@@ -0,0 +1,180 @@
+package com.flightbooking.activies;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.flightbooking.R;
+import com.flightbooking.api.ApiService;
+import com.flightbooking.model.ResponseData;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import pub.devrel.easypermissions.EasyPermissions;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AddHotelActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+    EditText etHotelName,etCountry,etProvince,etCity,etPostalCode,etPrice;
+    Button btnAddHotel,btn_upload;
+    ProgressDialog pd;
+    private static final String TAG = AddHotelActivity.class.getSimpleName();
+    private static final int REQUEST_GALLERY_CODE = 200;
+    private static final int READ_REQUEST_CODE = 300;
+    private static final String SERVER_PATH = "http://bookingflight.info/";
+    private Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hotel);
+
+        getSupportActionBar().setTitle("Add Hotel");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        etHotelName=(EditText)findViewById(R.id.etHotelName);
+        etCountry=(EditText)findViewById(R.id.etCountry);
+        etProvince=(EditText)findViewById(R.id.etProvince);
+        etCity=(EditText)findViewById(R.id.etCity);
+        etPostalCode=(EditText)findViewById(R.id.etPostalCode);
+        etPrice=(EditText)findViewById(R.id.etPrice);
+
+        btn_upload=(Button)findViewById(R.id.btn_upload);
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK);
+                openGalleryIntent.setType("image/*");
+                startActivityForResult(openGalleryIntent, REQUEST_GALLERY_CODE);
+
+            }
+        });
+
+        btnAddHotel=(Button)findViewById(R.id.btnAddHotel);
+        btnAddHotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImageToServer();
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, AddHotelActivity.this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_GALLERY_CODE && resultCode == Activity.RESULT_OK){
+            uri = data.getData();
+            if(EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                String filePath = getRealPathFromURIPath(uri, AddHotelActivity.this);
+                file = new File(filePath);
+
+            }else{
+                EasyPermissions.requestPermissions(this, getString(R.string.read_file), READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
+        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
+    }
+    File file;
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if(uri != null){
+            String filePath = getRealPathFromURIPath(uri, AddHotelActivity.this);
+            file = new File(filePath);
+            // uploadImageToServer();
+        }
+    }
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "Permission has been denied");
+
+    }
+
+    private void uploadImageToServer() {
+        pd = new ProgressDialog(AddHotelActivity.this);
+        pd.setTitle("Loading");
+        pd.show();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", etHotelName.getText().toString());
+        map.put("country", etCountry.getText().toString());
+        map.put("city", etCity.getText().toString());
+        map.put("province", etProvince.getText().toString());
+        map.put("pcode", etPostalCode.getText().toString());
+        map.put("price", etPrice.getText().toString());
+
+
+        RequestBody mFile = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("file", file.getName(), mFile);
+        RequestBody filename = RequestBody.create(MediaType.parse("text/plain"), file.getName());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(SERVER_PATH)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiService uploadImage = retrofit.create(ApiService.class);
+        Call<ResponseData> fileUpload = uploadImage.addhotel(fileToUpload, map);
+        fileUpload.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                pd.dismiss();
+                Toast.makeText(AddHotelActivity.this, "Hotel Added successfully. ", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(AddHotelActivity.this, HotelInfoActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                pd.dismiss();
+                Toast.makeText(AddHotelActivity.this, "Error" + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
