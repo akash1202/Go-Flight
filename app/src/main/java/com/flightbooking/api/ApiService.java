ab76d39351b3f74933e63dc7eadccc9b74df58e5
akash1202 <anaghanakash007@gmail.com>
2021-03-05 12:41:14 -0500

    hotel fragmeent adapter added

--- app/src/main/java/com/flightbooking/api/ApiService.java
+++ app/src/main/java/com/flightbooking/api/ApiService.java
@@ -61,4 +61,103 @@ public interface ApiService {
             @Query("price") String price);
 
 
+    @GET("/flight/adminlogin.php?")
+    Call<ResponseData> adminlogin(
+            @Query("uname") String uname,
+            @Query("pass") String pass);
+
+    @GET("/flight/getProfile.php?")
+    Call<List<MyProfilePojo>> getProfile(
+            @Query("email") String email);
+
+    @GET("/flight/mybookings.php?")
+    Call<List<BookingsPojo>> getmybooking(
+            @Query("email") String email);
+
+    @GET("/flight/update_profile.php?")
+    Call<ResponseData> update_profile(
+            @Query("fname") String fname,
+            @Query("lname") String lname,
+            @Query("email") String email,
+            @Query("pass") String pass,
+            @Query("phone") String phone);
+
+    @GET("flight/getusers.php?")
+    Call<List<MyProfilePojo>> getcustprofile();
+
+
+    @GET("flight/gethotel.php?")
+    Call<List<HotelInfoPojo>> gethotel();
+
+    @GET("flight/getbooking.php?")
+    Call<List<BookingsPojo>> getbooking();
+
+
+    @GET("flight/getroutes.php?")
+    Call<List<RouteInfoPojo>> getroutes();
+
+
+    @GET("flight/searchflight.php?")
+    Call<List<AvailableFlightsPojo>> getAvailableFlightRoutes(
+            @Query("source") String source,
+            @Query("destination") String destination,
+            @Query("type") String type);
+
+    @GET("flight/forgotPassword.php")
+    Call<ResponseData> forgotPassword(@Query("email") String emailid);
+
+    @Multipart
+    @POST("flight/addhotel.php")
+    Call<ResponseData> addhotel(
+            @Part MultipartBody.Part file,
+            @PartMap Map<String, String> partMap
+    );
+
+    @GET("/flight/booking.php?")
+    Call<ResponseData> booking(
+            @Query("rid") String rid,
+            @Query("name") String name,
+            @Query("passno") String passno,
+            @Query("passcountry") String passcountry,
+            @Query("dat") String dat,
+            @Query("extra") String extra,
+            @Query("clas") String clas,
+            @Query("child") String child,
+            @Query("adult") String adult,
+            @Query("total") String total,
+            @Query("uname") String uname
+    );
+
+//name,cno,xdate,cvv,rid,jdate,amount,uname
+    @GET("/flight/payment.php?")
+    Call<ResponseData> payment(
+            @Query("name") String name,
+            @Query("cno") String cno,
+            @Query("xdate") String xdate,
+            @Query("cvv") String cvv,
+            @Query("rid") String rid,
+            @Query("jdate") String jdate,
+            @Query("amount") String amount,
+            @Query("uname") String uname
+
+    );
+
+    @GET("/flight/deleteroute.php")
+    Call<ResponseData> deleteroute(@Query("rid") String rid);
+
+    @GET("/flight/editroute.php?")
+    Call<ResponseData> editroute(
+            @Query("source") String source,
+            @Query("destination") String destination,
+            @Query("airport") String airport,
+            @Query("airways") String airways,
+            @Query("frmtim") String frmtim,
+            @Query("totim") String totim,
+            @Query("tdays") String tdays,
+            @Query("type") String type,
+            @Query("stops") String stops,
+            @Query("layour") String layour,
+            @Query("rid") String rid,
+            @Query("price") String price);
+
 }
