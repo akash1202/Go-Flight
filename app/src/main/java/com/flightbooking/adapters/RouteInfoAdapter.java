c555e13076650d652a8618802188b0e7657cf931
abhishekp3099 <ronu4316@gmail.com>
2021-03-05 12:28:53 -0500

    Route info adapter changed

--- app/src/main/java/com/flightbooking/adapters/RouteInfoAdapter.java
+++ app/src/main/java/com/flightbooking/adapters/RouteInfoAdapter.java
@@ -1,6 +1,8 @@
 package com.flightbooking.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.flightbooking.R;
+import com.flightbooking.activies.AdminDashboardActivity;
+import com.flightbooking.activies.EditRouteActivity;
+import com.flightbooking.api.ApiService;
+import com.flightbooking.api.RetroClient;
+import com.flightbooking.model.ResponseData;
 import com.flightbooking.model.RouteInfoPojo;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RouteInfoAdapter extends BaseAdapter {
     List<RouteInfoPojo> routeInfoPojo;
@@ -65,7 +76,37 @@ public class RouteInfoAdapter extends BaseAdapter {
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(cnt, "Comin Soon", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(cnt, "Comin Soon", Toast.LENGTH_SHORT).show();
+
+
+//                @Query("source") String source,
+//                @Query("destination") String destination,
+//                @Query("airport") String airport,
+//                @Query("airways") String airways,
+//                @Query("frmtim") String frmtim,
+//                @Query("totim") String totim,
+//                @Query("tdays") String tdays,
+//                @Query("type") String type,
+//                @Query("stops") String stops,
+//                @Query("layour") String layour,
+//                @Query("rid") String rid,
+//                @Query("price") String price);
+
+
+                Intent intent=new Intent(cnt, EditRouteActivity.class);
+                intent.putExtra("source",routeInfoPojo.get(pos).getSource());
+                intent.putExtra("destination",routeInfoPojo.get(pos).getDestination());
+                intent.putExtra("airport",routeInfoPojo.get(pos).getAirport());
+                intent.putExtra("airways",routeInfoPojo.get(pos).getAirways());
+                intent.putExtra("frmtim",routeInfoPojo.get(pos).getFrmtim());
+                intent.putExtra("totim",routeInfoPojo.get(pos).getTotim());
+                intent.putExtra("tdays",routeInfoPojo.get(pos).getTdays());
+                intent.putExtra("type",routeInfoPojo.get(pos).getType());
+                intent.putExtra("stops",routeInfoPojo.get(pos).getStops());
+                intent.putExtra("layour",routeInfoPojo.get(pos).getLayour());
+                intent.putExtra("rid",routeInfoPojo.get(pos).getRid());
+                intent.putExtra("price",routeInfoPojo.get(pos).getPrice());
+                cnt.startActivity(intent);
             }
         });
 
@@ -73,11 +114,41 @@ public class RouteInfoAdapter extends BaseAdapter {
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(cnt, "Comin Soon", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(cnt, "Comin Soon", Toast.LENGTH_SHORT).show();
+                deleteRoute(routeInfoPojo.get(pos).getRid());
+
             }
         });
 
         return obj2;
+    }
+    ProgressDialog progressDialog;
+    public void deleteRoute(String rid){
+        progressDialog = new ProgressDialog(cnt);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = service.deleteroute(rid);
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                progressDialog.dismiss();
+                if(response.body()==null){
+                    Toast.makeText(cnt,"Server issue",Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent intent=new Intent(cnt, AdminDashboardActivity.class);
+                    cnt.startActivity(intent);
+                    Toast.makeText(cnt," Deleted successfully",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(cnt, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
