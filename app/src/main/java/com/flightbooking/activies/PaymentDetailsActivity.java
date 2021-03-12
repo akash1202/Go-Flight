e20118b90eb88d286ac26e917a2c3bf0c53c10bc
iamchaudharysudhir <chaudhary.sk2208@gmail.com>
2021-03-12 11:58:29 -0500

    ticket details and payment details changes

--- app/src/main/java/com/flightbooking/activies/PaymentDetailsActivity.java
+++ app/src/main/java/com/flightbooking/activies/PaymentDetailsActivity.java
@@ -1,24 +1,42 @@
 package com.flightbooking.activies;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.flightbooking.R;
 import com.flightbooking.Utils;
 import com.flightbooking.api.ApiService;
 import com.flightbooking.api.RetroClient;
 import com.flightbooking.model.ResponseData;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+import com.vinaygaba.creditcardview.CreditCardView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,79 +48,130 @@ public class PaymentDetailsActivity extends AppCompatActivity {
     Button btnCompleteBooking;
     ProgressDialog progressDialog;
     SharedPreferences sharedPreferences;
-    String uname;
+    CreditCardView debitCardView;
+    CardView debitLayout;
+    RadioButton radioDebit,radioPaypal;
+    String uname,paypalCreateTime,paypalPaymentID,paypalStatus;
+    private static PayPalConfiguration paypalconfig=new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_NO_NETWORK)
+            .merchantName("Go Flight")
+            .merchantPrivacyPolicyUri(Uri.parse("https://goflightinfo.000webhostapp.com/privacy-policy/"))
+            .merchantUserAgreementUri(Uri.parse("https://goflightinfo.000webhostapp.com/privacy-policy/"))
+            .clientId("AX8AoTzYqoQzwkrIe2A0pv-X1IEkP30C5DRZ5dPE9HuS7Zctn6rIdXk9u9nk6b5AmsEaGZYcXUELuZFE");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
-
-
         getSupportActionBar().setTitle("Payment Details");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        debitCardView=findViewById(R.id.debitCardView);
+        debitCardView.setBrandLogo(R.drawable.logo);
 
-        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
-        uname = sharedPreferences.getString("user_name", "");
         etNameonCard=(EditText) findViewById(R.id.etNameonCard);
         etCardNumber=(EditText)findViewById(R.id.etCardNumber);
         etExapDate=(EditText)findViewById(R.id.etExapDate);
         etCvvumber=(EditText)findViewById(R.id.etCvvumber);
 
+        radioDebit=findViewById(R.id.payOptionDebit);
+        radioPaypal=findViewById(R.id.payOPtionPaypal);
+
         tvName=(TextView)findViewById(R.id.tvName);
         tvDate=(TextView)findViewById(R.id.tvDate);
         tvTotalFair=(TextView)findViewById(R.id.tvTotalFair);
         tvRid=(TextView)findViewById(R.id.tvRid);
+        debitLayout=findViewById(R.id.debitCardLayout);
+
+        btnCompleteBooking=(Button)findViewById(R.id.btnCompleteBooking);
+        showDebitOPtion();
+
+        radioDebit.setChecked(true);
+        radioPaypal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    showPaypalPayOption();
+                }
+            }
+        });
+        radioDebit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    showDebitOPtion();
+                }
+            }
+        });
+
+        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        uname = sharedPreferences.getString("user_name", "");
+        debitCardView.setCardName(getIntent().getStringExtra("passengername"));
+        debitCardView.setExpiryDate("MM/YY");
 
         tvRid.setText("Route Id:  ROU#"+getIntent().getStringExtra("Id"));
         tvName.setText("Name:  "+getIntent().getStringExtra("passengername"));
         tvDate.setText("Journy Date:  "+getIntent().getStringExtra("journydate"));
-        tvTotalFair.setText("Total Fair:  "+getIntent().getStringExtra("totalfair")+"CAD");
-
-        btnCompleteBooking=(Button)findViewById(R.id.btnCompleteBooking);
+        tvTotalFair.setText("Total Fair:  "+getIntent().getStringExtra("totalfair"));
         btnCompleteBooking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                // Toast.makeText(PaymentDetailsActivity.this, "Succuslly Received your Payment", Toast.LENGTH_SHORT).show();
 
-                if(etNameonCard.getText().toString().isEmpty()){
+                if(debitCardView.getCardName().toString().isEmpty()){
                     Toast.makeText(PaymentDetailsActivity.this, "Please Enter name..", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(etCardNumber.getText().toString().isEmpty()){
+                if(debitCardView.getCardNumber().toString().isEmpty()){
                     Toast.makeText(PaymentDetailsActivity.this, "Please Enter Card Number..", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(etExapDate.getText().toString().isEmpty()){
+                if(debitCardView.getExpiryDate().toString().isEmpty()){
                     Toast.makeText(PaymentDetailsActivity.this, "Please Enter Exp Date..", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                if(etCvvumber.getText().toString().isEmpty()){
-                    Toast.makeText(PaymentDetailsActivity.this, "Please Enter Cvv", Toast.LENGTH_SHORT).show();
-                    return;
+                if(radioDebit.isChecked()){
+                    payment(); //pay with debit card
+                }else{
+                    payNowWithPaypal(); //pay with credit card
                 }
-
-                payment();
             }
         });
+    }
+
+    public void showPaypalPayOption(){
+            debitLayout.setVisibility(View.GONE);
+            btnCompleteBooking.setText("Continue Booking");
+    }
+    public void showDebitOPtion(){
+        debitLayout.setVisibility(View.VISIBLE);
+        btnCompleteBooking.setText("Complete Booking");
     }
 
     //$query_dis="insert into book(name,cno,xdate,cvv,amount,jdate,email,rid) values('$name','$cno','$xdate','$cvv',
     //'$amount','$jdate','$email','$rid');";
 
-    private void payment() {
-
+    private void payment() { //debit card payment
         final String rid = getIntent().getStringExtra("Id");
         final String passengername = getIntent().getStringExtra("passengername");
         final String jdate = getIntent().getStringExtra("journydate");
         final String amount = getIntent().getStringExtra("totalfair");
-
+/*
         String name = etNameonCard.getText().toString();
         String cno = etCardNumber.getText().toString();
         String xdate = etExapDate.getText().toString();
-        String cvv = etCvvumber.getText().toString();
+        String cvv = etCvvumber.getText().toString();*/
+
+        String name = debitCardView.getCardName();
+        String cno = debitCardView.getCardNumber();
+        String xdate = debitCardView.getExpiryDate();
+        String cvv="210";
+        debitCardView.setIsCvvEditable(true);
+
+
         progressDialog = new ProgressDialog(PaymentDetailsActivity.this);
-        progressDialog.setMessage("Please Wait wr are processing your payment....");
+        progressDialog.setMessage("Please Wait we are processing your payment....");
         progressDialog.show();
 
         ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
@@ -116,7 +185,6 @@ public class PaymentDetailsActivity extends AppCompatActivity {
                     Intent intent = new Intent(PaymentDetailsActivity.this, CustomerDasboardActivity.class);
                     startActivity(intent);
                     finish();
-
                 } else {
                     Toast.makeText(PaymentDetailsActivity.this, response.body().message, Toast.LENGTH_LONG).show();
                 }
@@ -129,6 +197,65 @@ public class PaymentDetailsActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void payNowWithPaypal(){ //credit card & paypal
+        Intent intent=new Intent(PaymentDetailsActivity.this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,paypalconfig);
+        startService(intent);
+        //PAYMENT_INTENT_SALE will cause the payment to complete immediately
+        // change PAYMENT_INTENT_AUTHORIZE to only authorize payment and capture funds later
+        //change PAYMENT_INTENT_ORDER to create a payment for
+        PayPalPayment payPalPayment=new PayPalPayment(new BigDecimal(getIntent().getStringExtra("totalfair")),
+                "CAD","Akash Anaghan",PayPalPayment.PAYMENT_INTENT_ORDER);
+        Intent intent1=new Intent(this, PaymentActivity.class);
+        intent1.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,paypalconfig);
+        intent1.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+        startActivityForResult(intent1,0);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode== Activity.RESULT_OK){
+            PaymentConfirmation paymentConfirmation=data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+            if(paymentConfirmation!=null){
+                try{
+                    Log.i("Paypal response",paymentConfirmation.toJSONObject().toString(4));
+
+                    Toast.makeText(this, paymentConfirmation.toJSONObject().toString(4), Toast.LENGTH_LONG).show();
+                    if(isPaymentSuccess(paymentConfirmation.toJSONObject())){
+                        finish();
+                    }
+                    //Toast.makeText(this, "Payment Done !!!", Toast.LENGTH_LONG).show();
+                    //setText(paymentConfirmation.toJSONObject().toString(4));
+                    //TODO: send 'confirmation to server for verification'
+                }catch (JSONException e){
+                    Log.e("Paypal JSONError:",e.toString());
+                }
+            }
+        }else if(resultCode==Activity.RESULT_CANCELED){
+            Log.i("Paypal ","User has canceled");
+        }else if(resultCode==PaymentActivity.RESULT_EXTRAS_INVALID){
+            Log.i("Paypal Example","An invalid Paypal Configuration");
+        }
+    }
+
+    boolean isPaymentSuccess(JSONObject response){
+        try {
+            JSONObject clientData= response.getJSONObject("client");
+            JSONObject responseData=response.getJSONObject("response");
+            paypalCreateTime=responseData.getString("create_time");
+            paypalPaymentID=responseData.getString("id");
+            paypalStatus=responseData.getString("state");
+            if(paypalStatus.equalsIgnoreCase("approved")){
+                Toast.makeText(getApplicationContext(), "Payment Successful !!!", Toast.LENGTH_LONG).show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
 
 
 
